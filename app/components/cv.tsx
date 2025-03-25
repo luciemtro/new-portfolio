65bd122d9f64cfc59e0776bdb479e9dd9720ce2f
Lucie Monteir <lucie.mtro@gmail.com>
2025-03-25 22:15:27 +0100

    Add cyber button styling to CV component; update download link and enhance global styles

--- app/components/cv.tsx
+++ app/components/cv.tsx
@@ -4,13 +4,19 @@ export default function Cv() {
       <h2 className="font-title text-2xl text-center p-10">
         Mon Curriculum Vitae
       </h2>
-      <div className="flex items-center justify-center min-h-[50vh]">
+      <div className="flex items-center justify-center  min-h-[50vh]">
         <a
           href="https://github.com/luciemtro/portefolio/raw/main/app/components/cvluciemonteiro.pdf"
           download="cvluciemonteiro.pdf"
-          className="button puce--violet uppercase font-basic scale "
         >
-          Télécharger mon cv en PDF
+          <button className="cybr-btn">
+            Download
+            <span aria-hidden>_</span>
+            <span aria-hidden className="cybr-btn__glitch">
+              Download
+            </span>
+            <span aria-hidden className="cybr-btn__tag"></span>
+          </button>
         </a>
       </div>
     </section>
