3f21a2546d40bb3fc0303b1ce88eb97b42ce6c15
Lucie Monteir <lucie.mtro@gmail.com>
2025-08-22 14:04:07 +0200

    Add Project Kababi

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -27,11 +27,23 @@ export default function Projects() {
             <h3 className="title-project">Drink Saver</h3>
           </div>
         </div>
-        <div className="card-project lunova">
-          <div className="flex justify-center items-center container-title-project">
-            <h3 className="title-project">Lunova</h3>
-          </div>
-        </div>
+        <article className="flex flex-col items-center gap-4">
+          <a href="https://kababi-xi.vercel.app" target="_blank">
+            <div className="card-project kababi">
+              <div className="flex justify-center items-center container-title-project">
+                <h3 className="title-project">Kababi</h3>
+              </div>
+            </div>
+          </a>
+          <a
+            href="https://github.com/luciemtro/Kababi"
+            className="cursor-pointer hover:scale-125 transform transition-transform duration-500 ease-in-out hover:text-white flex items-center justify-center gap-2"
+            target="_blank"
+          >
+            <FaGithub size={30} />
+            <span className="metallic-text ">Github</span>
+          </a>
+        </article>
       </div>
     </section>
   );
